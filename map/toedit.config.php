2d11c006994b7bd297d2bfd2a1d9f69e60e9d85d
GradelerM <marie.gradeler@gmail.com>
2021-05-28 14:31:55 +0200

    added toedit.config.php

---
+++ map/toedit.config.php
@@ -0,0 +1,12 @@
+<?php
+
+// Edit the file with the real values (see .env in docker-compose)
+// Rename this file as config.php once you're done (it will be protected by .gitignore)
+
+$_SESSION['db_host']='localhost';
+$_SESSION['db_port']='5431';
+$_SESSION['db_name']='namo';
+$_SESSION['db_user']='webconnexion';
+$_SESSION['db_password']='webconnect';
+
+?>
