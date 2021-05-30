8f771702d740ec9b5449770a950568a1d34faedb
GradelerM <marie.gradeler@gmail.com>
2021-05-30 22:25:13 +0200

    testing SVG logos and landing page

--- index.php
+++ index.php
@@ -22,83 +22,30 @@
 
     <body>
         <!-- Grid container -->
-        <div class="container">
-            <div id="header">
-                <div id="header-info">
-                    <img src="./landingpage/logoHorizontal.png" />
-                    <p>
-                        Innover en agroécologie pour gérer, préserver et restaurer 
-                        la qualité environnementale du territoire - <br />
-                        atlas interactif et cartographie narrative des enjeux environnementaux 
-                        de l'agriculture guadeloupéenne.
-                    </p>
-                    <div class="a-button">
-                        <a href="./map/map.php" class="cta">Explorer</a>
-                    </div>
+        <div class="wrapper">
+            <div class="container">
+                
+                <div class="logo">
+                <?php echo '<img src="images/assets/logos/'.$horizontal_logo.'" title="logo" alt="logo" />'; ?>
                 </div>
-            </div>
-            <div id="presentation-txt">
-                <h1>Présentation</h1>
-                <p>
-                    Le projet RIVAGE, coordonné par le Cirad, propose de mettre en place un dispositif 
-                    sociotechnique d'accompagnement de la transition écologique de l'agriculture guadeloupéenne. 
-                    Ce dispositif vise à accompagner les agriculteurs d'un territoire dans l'adoption de pratiques 
-                    agro-écologiques, et à faciliter l'intervention des acteurs locaux dans la gestion de l'environnement. 
-                    Il s'agira de mieux connaître l'activité agricole et ses déterminants à l'échelle individuelle 
-                    et à celle du territoire ; d'apporter des connaissances sur le fonctionnement du milieu physique 
-                    et d'établir un diagnostic de l'impact des pratiques sur les différents compartiments sols, nappes 
-                    et rivières à l'échelle du bassin versant.
-                </p>
-            </div>
-            <div id="presentation-img">Présentation image</div>
-            <div id="content1-txt">
-                <h1>Titre</h1>
-                <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam repellendus maiores veniam cumque deleniti amet, sed commodi doloribus aliquid nemo impedit atque suscipit quaerat maxime. Molestias libero quam eaque nesciunt!
-                </p>
-            </div>
-            <div id="content1-img">Contenu 1 image</div>
-            <div id="content2-txt">
-                <h1>Partenaires</h1>
-                <p>
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dicta doloremque rem, dolor incidunt blanditiis ut expedita minima quae tenetur fugiat aliquid aspernatur mollitia similique officiis minus iusto aut, molestias cum. 
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nisi facilis voluptate non adipisci, ad iusto eius praesentium est! At velit facere odio dolore, iure voluptatem qui nihil officia? Veritatis, aperiam.
-                </p>
-            </div>
-            <div id="content2-img">Contenu 2 image</div>
-            <div id="go-to-map">
-                <div class="a-button">
-                    <a href="./map/map.php" class="cta">Explorer la carte interactive</a>
-                </div>
-            </div>
-            <footer>
-                <div id="link-gitlab">
-                    <h3>Documentation et code source</h3>
-                    <div>
-                        <a href="https://gitlab.com/GradelerM/rivage-guadeloupe"><img src="./landingpage/gitlab.png"/></a>
-                        <a class="reflink" href="https://gitlab.com/GradelerM/rivage-guadeloupe">Dépôt GitLab</a>
-                    </div>
-                    <hr id="hr-gitlab">
-                </div>
-                <div id="links-info">
-                    <h3>Plus d'informations</h3>
-                    <a href="https://antilles-guyane.cirad.fr/recherche-en-partenariat/projets-finances-par-l-europe/rivage-innover-en-agroecologie">Le projet Rivage : site du Cirad</a>
-                    <hr id="hr-info">
-                </div>
-                <div id="contact-info">
-                    <h3>Contact</h3>
-                    <p><b>Cirad</b></p>
-                    <p><b>UMR Tetis</b></p>
-                    <p>
-                        Maison de la télédétection <br />
-                        500 rue Jean-François Breton 34093 Montpellier cedex 5 <br />
-                        Tel. (Standard) : (+33) 04 67 54 87 54 <br />
-                        E-mail : aurelie.dourdain@cirad.fr
-                    </p>
-                </div>
-            </footer> 
-        </div>
 
+                <div class="text">
+                    <p><b>Bienvenue sur la plateforme de cartographie narrative NAMO GeoWeb</b></p>
+                    <p>
+                        La plateforme NAMO est un espace collaboratif pour la rédaction et la publication de 
+                        cartes narratives pour documenter un thème, valoriser un projet, etc. Pour commencer 
+                        à explorer la carte, cliquez sur le bouton ci-dessous.
+                    </p>
+                    <p>Bonne exploration !</p>
+                    <?php echo $horizontal_logo; ?>
+                </div>
+
+                <div class="button">
+                    <a href="./map/map.php"><button class="mainButton positivebtn">Accéder à la carte</button></a>
+                </div>
+
+            </div>
+        </div>
 
     </body>
 
