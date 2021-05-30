e33a010ee4d9202f23f1d435ff5bef52c0820fd0
GradelerM <marie.gradeler@gmail.com>
2021-05-30 12:41:34 +0200

    added new config variables with project name and logo paths, importing info modal content from php component, need to test before processing further

--- map/toedit.config.php
+++ map/toedit.config.php
@@ -1,12 +1,30 @@
 <?php
-
-// Edit the file with the real values (see .env in docker-compose)
+// Edit the file with values matching (see .env in docker-compose)
 // Rename this file as config.php once you're done (it will be protected by .gitignore)
 
+// ====================================================================================
+// PROJECT INFORMATIONS
+// ====================================================================================
+// Project name and logos
+$project_name='Rivage';
+$vertical_logo='verticalLogo.png';
+$horizontal_logo='horizontalLogo.png';
+
+// Noreply mail origin (can keep default)
+$noreply_mail='noreply@namo'.$project_name.'.com';
+
+// Link to repository
+// Default: NAMO official repositories
+// Change if you have your own NAMO repository (for your specific project)
+$repo_link='https://github.com/GradelerM/NAMOgeoweb';
+$wiki_link='https://github.com/GradelerM/NAMOgeoweb/wiki';
+
+// ====================================================================================
+// DATABASE INFORMATIONS
+// ====================================================================================
 $_SESSION['db_host']='localhost';
 $_SESSION['db_port']='5431';
 $_SESSION['db_name']='namo';
 $_SESSION['db_user']='webconnexion';
 $_SESSION['db_password']='webconnect';
-
 ?>
